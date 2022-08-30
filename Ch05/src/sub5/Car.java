5c8a16752d2ff5c5aee8afc46bcf9321595f510c
bomberman114 <mielmer016@gmail.com>
2022-08-30 16:22:56 +0900

    2022/08/30 상속예제 추가

---
+++ Ch05/src/sub5/Car.java
@@ -0,0 +1,59 @@
+package sub5;
+
+public class Car { // class는 교실과 그룹이라는 뜻이 있다.
+
+	// 속성
+	protected String name; // 속성과 기능들을 맴버라고 한다.
+	protected String color;
+	protected int speed;
+
+	// 생성자
+	public Car(String name, String color, int speed) {
+		this.name = name;
+		this.color = color;
+		this.speed = speed;
+	}
+
+	// Getter, Setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	// 기능
+	public void speedUp(int speed) {
+		this.speed += speed;
+
+	}
+
+	public void speedDwon(int speed) {
+		this.speed -= speed;
+
+	}
+
+	public void show() {
+		System.out.println("차량명: " + name);
+		System.out.println("차량색: " + color);
+		System.out.println("현재속도: " + speed);
+	}
+
+}
